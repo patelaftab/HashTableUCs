70460cdce483906eebf996216724e66f3ff7a17f
patelaftab <10patelaftab@gmail.com>
2023-01-01 21:02:40 +0530

    [Aftab] Removing Avoidable Words From Paragraphs

--- HashTableUCs/Program.cs
+++ HashTableUCs/Program.cs
@@ -7,7 +7,8 @@
             Console.WriteLine("Welcome To HashTable Program");
             Console.WriteLine("Choose a Number From Below\n" +
                 "1: Checking a Frequency For Single Line Statement (UC1)\n" +
-                "2: Checking a Frequency For Paragraph Statement (UC2)\n");
+                "2: Checking a Frequency For Paragraph Statement (UC2)\n" +
+                "3: Removing word \\\"avoidable\\\" and recounting the occurrences in a same paragraph.\\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -22,6 +23,29 @@
                         "deliberately into paranoid avoidable " +
                         "situations";
                     CountNumbOfOccurence(paragraph1);
+                    break;
+                case 3:                     //UC3 (Counting frequency of occurrence of words in a paragraph and removing 1 word and showing difference.
+                    MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+                    string paragraph2 = "Paranoids are not paranoid " +
+                        "because they are paranoid but " +
+                        "because they keep putting themselves " +
+                        "deliberately into paranoid avoidable " +
+                        "situations";
+                    string[] words = paragraph2.Split(' ');
+                    foreach (string word in words)
+                    {
+                        if (hashTabe.Exists(word.ToLower()))
+                            hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                        else
+                            hashTabe.Add(word.ToLower(), 1); //to,1 
+                    }
+                    Console.WriteLine("All words counted .");
+                    hashTabe.Display();
+                    string s = "avoidable";
+                    hashTabe.Remove(s);
+                    Console.WriteLine();
+                    Console.WriteLine($"A word \"{s}\" has been removed ");
+                    hashTabe.Display();
                     break;
                 default:
                     Console.WriteLine("Invalid option selected ! Please try again .");
